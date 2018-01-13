bf6b5dafefe04e2e9398c1a09a6994a280d87e02
soulruji <soulruji@gmail.com>
2018-01-13 17:16:22 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function translator(str) {
+  return str.split('').map(function(char){
+    var d = Hangul.disassemble(char);
+    if(d[3] && Hangul.isVowel(d[1]) && Hangul.isVowel(d[2])) {
+      var temp = d[3];
+      d[3] = d[2];
+      d[2] = temp;
+    }
+    return Hangul.assemble(d);
+  }).join('');
+
+  console.log(d)
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('.change').addEventListener('click', function () {
+    var changeText = translator(document.querySelector('.original-text').value);
+    document.querySelector('.result-text').innerText = changeText;
+  })
+})
